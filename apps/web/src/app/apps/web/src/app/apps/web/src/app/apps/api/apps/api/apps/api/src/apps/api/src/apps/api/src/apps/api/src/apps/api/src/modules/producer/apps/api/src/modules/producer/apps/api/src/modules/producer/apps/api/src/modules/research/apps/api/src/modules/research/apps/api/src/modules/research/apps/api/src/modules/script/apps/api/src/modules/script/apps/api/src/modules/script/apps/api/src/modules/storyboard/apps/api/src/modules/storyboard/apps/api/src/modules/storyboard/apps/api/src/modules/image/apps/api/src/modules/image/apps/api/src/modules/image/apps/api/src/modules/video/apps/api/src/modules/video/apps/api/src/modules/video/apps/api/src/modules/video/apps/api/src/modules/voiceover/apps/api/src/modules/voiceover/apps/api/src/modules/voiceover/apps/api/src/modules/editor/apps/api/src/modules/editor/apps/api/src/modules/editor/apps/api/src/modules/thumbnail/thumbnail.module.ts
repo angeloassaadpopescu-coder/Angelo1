a2c4ea233195a3815034c4b9e09ee8c0d52ac3db
angeloassaadpopescu-coder <angeloassaadpopescu@gmail.com>
2026-06-04 21:37:06 -0400

    Create ThumbnailModule with controller and service

---
+++ apps/web/src/app/apps/web/src/app/apps/web/src/app/apps/api/apps/api/apps/api/src/apps/api/src/apps/api/src/apps/api/src/apps/api/src/modules/producer/apps/api/src/modules/producer/apps/api/src/modules/producer/apps/api/src/modules/research/apps/api/src/modules/research/apps/api/src/modules/research/apps/api/src/modules/script/apps/api/src/modules/script/apps/api/src/modules/script/apps/api/src/modules/storyboard/apps/api/src/modules/storyboard/apps/api/src/modules/storyboard/apps/api/src/modules/image/apps/api/src/modules/image/apps/api/src/modules/image/apps/api/src/modules/video/apps/api/src/modules/video/apps/api/src/modules/video/apps/api/src/modules/video/apps/api/src/modules/voiceover/apps/api/src/modules/voiceover/apps/api/src/modules/voiceover/apps/api/src/modules/editor/apps/api/src/modules/editor/apps/api/src/modules/editor/apps/api/src/modules/thumbnail/thumbnail.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { ThumbnailService } from './thumbnail.service';
+import { ThumbnailController } from './thumbnail.controller';
+
+@Module({
+  controllers: [ThumbnailController],
+  providers: [ThumbnailService],
+  exports: [ThumbnailService],
+})
+export class ThumbnailModule {}
